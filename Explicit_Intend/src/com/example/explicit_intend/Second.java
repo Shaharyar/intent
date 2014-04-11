fed7cba0ad65d10014eef7acb786d44b6c8c5d6c
shaharyar <shaharyar_92@rocketmail.com>
2014-04-11 21:55:10 +0500

    adding explicit intent example

---
+++ Explicit_Intend/src/com/example/explicit_intend/Second.java
@@ -0,0 +1,16 @@
+package com.example.explicit_intend;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Second extends Activity{
+	 protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.second);
+	        TextView tv=(TextView) findViewById(R.id.textView1);
+	        tv.setText(getIntent().getExtras().getString("sometext"));
+	        
+	 }
+
+}
